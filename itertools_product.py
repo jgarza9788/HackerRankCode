928d28eae30e6a5caf3717f367f4745c790585b1
jgarza9788 <jgarza9788@gmail.com>
2019-11-30 18:22:50 -0800

    itertools.product

--- itertools_product.py
+++ itertools_product.py
@@ -1,26 +1,5 @@
-
-
-# print('AABCAAADA'[0:][:3])
-
-def merge_the_tools0(string, k):
-    i = 0 
-    while i < len(string):
-        s = string[i:][:k]
-        fs = ''
-        for l in s:
-            if l in fs:
-                pass
-            else:
-                fs = fs+l
-        print(fs)
-        i+=k
-
-def merge_the_tools(string, k):
-    for part in zip(*[iter(string)] * k):
-        # print(part)
-        d = dict()
-        # print(d)
-        print(''.join([ d.setdefault(c, c) for c in part if c not in d ]))
+#itertools.product()
+#https://www.hackerrank.com/challenges/itertools-product/problem
 
 import itertools
 
